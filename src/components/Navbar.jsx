3d3003aee103085a1e49efac3da607ff0e93b9fd
Muhammad Saqib <sm4179515@gmail.com>
2024-10-04 14:56:04 +0500

    now nav bar is completed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -2,7 +2,18 @@ import logo from '../assets/foodLogo.jpg';
 
 const Navbar = () => {
   return (
-    <h1> now in the nav bar</h1>
+    <nav className="flex justify-between items-center px-5 py-2 sm:flex-row flex-col">
+        <div className="flex items-center">
+            <img className="w-[60px]" src={logo} alt="" />
+            <h2 className="font-bold text-gray-600 ml-2">WOW FOODS</h2>
+        </div>
+        <div className="flex gap-x-5 items-center">
+            <h2 className="text-gray-600 hover:text-pink-600 hover:font-semibold hover:cursor-pointer">Home</h2>
+            <h2 className="text-gray-600 hover:text-pink-600 hover:font-semibold hover:cursor-pointer">About</h2>
+            <h2 className="text-gray-600 hover:text-pink-600 hover:font-semibold hover:cursor-pointer">Foods</h2>
+            <h2 className="text-gray-600 hover:text-pink-600 hover:font-semibold hover:cursor-pointer">Contact</h2>
+        </div>
+    </nav>
   )
 }
 
